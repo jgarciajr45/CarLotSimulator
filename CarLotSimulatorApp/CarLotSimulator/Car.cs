daf470e219b1879510f992953ce00a9b4712f01d
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-11 14:21:54 -0700

    3CarsAdded

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+		public int Year { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public string EngineNoise { get; set; }
+		public string HonkNoise { get; set; }
+		public bool IsDriveable { get; set; }
+
+
+		public void MakeEngineNoise()
+		{
+			Console.WriteLine(EngineNoise);
+		}
+		public void MakeHonkNoise()
+		{
+			Console.WriteLine(HonkNoise);
+		}
+
+		public Car()
+		{
+		}
+	}
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -18,6 +18,42 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            Car car1 = new Car();
+            car1.Year = 2017;
+            car1.Make = "Land Rover";
+            car1.Model = "Discovery";
+            car1.EngineNoise = "Vroom";
+            car1.HonkNoise = "Beep Beep";
+            car1.IsDriveable = true;
+            Car car2 = new Car();
+            car2.Year = 2010;
+            car2.Make = "JEEP";
+            car2.Model = "Wrangler";
+            car2.EngineNoise = "Brrrrrrr";
+            car2.HonkNoise = "Honk Honk";
+            car2.IsDriveable = true;
+            Car car3 = new Car();
+            car3.Year = 1984;
+            car3.Make = "Man";
+            car3.Model = "Legs";
+            car3.EngineNoise = "Swish";
+            car3.HonkNoise = "MEEP MEEP";
+            car3.IsDriveable = false;
+
+            Console.WriteLine($"Car 1: {car1.Year} {car1.Make} {car1.Model}");
+            car1.MakeEngineNoise();
+            car1.MakeHonkNoise();
+
+            Console.WriteLine($"Car 2: {car2.Year} {car2.Make} {car2.Model}");
+            car2.MakeEngineNoise();
+            car2.MakeHonkNoise();
+
+            Console.WriteLine($"Car 3: {car3.Year} {car3.Make} {car3.Model}");
+            car3.MakeEngineNoise();
+            car3.MakeHonkNoise();
+
+
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
